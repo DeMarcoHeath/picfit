ad508a0be7e167a6a1a945b6e3bc29a5b1bdf56c
Chidinma Nze <chidinma.ogbuaku@gmail.com>
2020-11-04 21:40:26 -0500

    new file structure

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
@@ -9,12 +9,12 @@ function Detail(props) {
 
   // When this component mounts, grab the book with the _id of props.match.params.id
   // e.g. localhost:3000/books/599dcb67f0f16317844583fc
-  const {id} = useParams()
-  useEffect(() => {
-    API.getBook(id)
-      .then(res => setBook(res.data))
-      .catch(err => console.log(err));
-  }, [])
+  // const {id} = useParams()
+  // useEffect(() => {
+  //   API.getBook(id)
+  //     .then(res => setBook(res.data))
+  //     .catch(err => console.log(err));
+  // }, [])
 
   return (
       <Container fluid>
