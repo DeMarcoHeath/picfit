4c07ca5ca162e41b9c7a93390545c55ddeff79a1
Demarco Heath <demarcoheath@gmail.com>
2020-11-14 14:08:34 -0500

    index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build", "index.js"));
   });
 }
 
